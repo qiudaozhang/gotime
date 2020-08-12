bddacde14919e71a6583fe4cbed39b473ea2143c
13928421127 <singkingcho@gmail.com>
2020-08-12 17:18:18 +0800

    增加2006-01-02格式解析

--- util/time.go
+++ util/time.go
@@ -1,6 +1,5 @@
 package util
 
-
 import (
 	"log"
 	"time"
@@ -8,14 +7,25 @@ import (
 
 const (
 	LAYOUT20060102150405 = "2006-01-02 15:04:05"
+	LAYOUT20060102       = "2006-01-02"
 )
 
-func ParseRFC3339TimeYYMMDDHHMMSS(t string ) string {
+func ParseRFC3339TimeYYMMDDHHMMSS(t string) string {
 	//2006-01-02T15:04:05Z07:00 --> 2006-01-02 15:04:05
-	newTime ,err := time.Parse(time.RFC3339,t)
+	newTime, err := time.Parse(time.RFC3339, t)
 	if err != nil {
 		log.Println(err.Error())
 	}
 	formatTime := newTime.Format(LAYOUT20060102150405)
+	return formatTime
+}
+
+func ParseRFC3339TimeYYMMDD(t string) string {
+	//2006-01-02T15:04:05Z07:00 --> 2006-01-02
+	newTime, err := time.Parse(time.RFC3339, t)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	formatTime := newTime.Format(LAYOUT20060102)
 	return formatTime
 }
