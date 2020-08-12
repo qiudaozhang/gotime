f8707e69ab01de888fa0e1c08bbdc06e7ac6be2d
13928421127 <singkingcho@gmail.com>
2020-08-12 17:23:54 +0800

    增加15:04:05解析函数。

--- util/time.go
+++ util/time.go
@@ -8,6 +8,7 @@ import (
 const (
 	LAYOUT20060102150405 = "2006-01-02 15:04:05"
 	LAYOUT20060102       = "2006-01-02"
+	LAYOUT150405         = "15:04:05"
 )
 
 func ParseRFC3339TimeYYMMDDHHMMSS(t string) string {
@@ -27,5 +28,15 @@ func ParseRFC3339TimeYYMMDD(t string) string {
 		log.Println(err.Error())
 	}
 	formatTime := newTime.Format(LAYOUT20060102)
+	return formatTime
+}
+
+func ParseRFC3339TimeHHMMSS(t string) string {
+	//2006-01-02T15:04:05Z07:00 --> 15:04:05
+	newTime, err := time.Parse(time.RFC3339, t)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	formatTime := newTime.Format(LAYOUT150405)
 	return formatTime
 }
