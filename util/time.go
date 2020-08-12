e4255f786bec2fe8690991400b74742629394ed0
13928421127 <singkingcho@gmail.com>
2020-08-12 17:10:34 +0800

    Initial commit

---
+++ util/time.go
@@ -0,0 +1,21 @@
+package util
+
+
+import (
+	"log"
+	"time"
+)
+
+const (
+	LAYOUT20060102150405 = "2006-01-02 15:04:05"
+)
+
+func ParseRFC3339TimeYYMMDDHHMMSS(t string ) string {
+	//2006-01-02T15:04:05Z07:00 --> 2006-01-02 15:04:05
+	newTime ,err := time.Parse(time.RFC3339,t)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	formatTime := newTime.Format(LAYOUT20060102150405)
+	return formatTime
+}
